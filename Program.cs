b86d894f37b127f0896f5e32596c4415671a825a
Edward Angeles <eangelesluza5570@conestogac.on.ca>
2024-11-04 17:35:21 -0500

    Addition of the removeStopWords function

--- Program.cs
+++ Program.cs
@@ -63,4 +63,43 @@
 
         return answerType;
     }
+
+    static string removeStopWords(string question)
+    { //remove stop words from the question?
+
+        int i = 0;
+        bool isStopWord = false;
+        string[] stopWords = { "and", "is", "are", "the", "a", "was", "in", "on" };
+        string questionWithoutStopWords = " ";
+        string word = "";
+
+        while (question[i] != '?')
+        {
+
+            if (question[i] != ' ')
+            {
+                word += question[i];
+            }
+
+            else if (question[i] == ' ')
+            {
+                word = ""; //empty the word for nextround
+                for (int w = 0; w < stopWords.Length; w++)
+                {
+                    if (word == stopWords[w])
+                    {
+                        isStopWord = true;
+                        break;
+                    }
+                }
+                if (!isStopWord)
+                {
+                    questionWithoutStopWords += word;
+                }
+            }
+
+            i++;
+        }
+
+    }
 }
