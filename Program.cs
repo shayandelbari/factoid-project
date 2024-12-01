a6e17f9341ceebabfc9c286329508bc6ff8b9b27
Brett Trudel <trudel25@gmail.com>
2024-12-01 17:00:30 -0500

    Replaced foreach loop to print answer with for loop

--- Program.cs
+++ Program.cs
@@ -62,9 +62,10 @@
                     if (answers is null) Console.WriteLine("You're question didn't have any answers could you rephrase it?");
                     else
                     {
-                        foreach (string answer in answers)
+                        Console.WriteLine();
+                        for (int i = 0; i >= answers.Length; i++)
                         {
-                            Console.WriteLine(answer);
+                            Console.WriteLine(answers[i]);
                         }
                     }
                     Console.ReadKey();
